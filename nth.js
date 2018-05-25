a4e56da3d91c1a9c62154c85b7a49eac02bda8f3
Emmalauren sambu <eparlaurensanbu@gmail.com>
2018-05-25 16:03:51 +0300

    modified

--- nth.js
+++ nth.js
@@ -2,7 +2,18 @@ function arrayList(array) {
 
   var list = null;
   
-  for (var i = array.length - 1; i >= 0; i--)
+  for (var i = array.length - 1; i >= 0; i--);
     list = {value: array[i], rest: list};
   return list;
 }
+
+function nth(list, x) {
+  if (!list)
+    return undefined;
+  else if (x == 0)
+    return list.value;
+  else
+    return nth(list.rest, x - 1);
+}
+
+console.log(nth(arrayToList([30, 40, 56, 100, 90, 10]), 5));
